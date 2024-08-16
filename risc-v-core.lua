602aaebe53a77e93a165cc5548a66e68dcfd903b
vladvis <vladas.bulavas@gmail.com>
2024-08-16 17:13:35 +0300

    decoding of all instruction types was implemented

--- risc-v-core.lua
+++ risc-v-core.lua
@@ -9,6 +9,30 @@ local function bin(x)
     return tonumber(x, 2)
 end
 
+local function decode_rd(instr)
+    return bit.band(bit.rshift(instruction, 7), 0x1f) -- (instr >> 7) & 0b11111
+end
+
+local function decode_rs1(instr)
+    return bit.band(bit.rshift(instruction, 15), 0x1f) -- (instr >> 15) & 0b11111
+end
+
+local function decode_rs2(instr)
+    return bit.band(bit.rshift(instruction, 20), 0x1f) -- (instr >> 20) & 0b11111
+end
+
+local function decode_funct3(instr)
+    return bit.band(bit.rshift(instruction, 12), 0x07) -- (instr >> 12) & 0b111
+end
+
+local function set_sign(value, bits)
+    local max_int = bit.lshift(1, bits - 1) - 1
+    if value > max_int then
+        return value - (max_int + 1)*2
+    end
+    return value
+end
+
 function RiscVCore:LoadRegister(source)
     assert((dest >= 0) and (dest <= 31), "register x".. tostring(source) .." isn't existed (load)")
     return self.registers[source]
@@ -26,6 +50,21 @@ function RiscVCore:StorePC(value)
     self.registers.pc = bit.band(value, 0xffffffff)
 end
 
+function RiscVCore:ReadDWORD(address)
+    assert((address >= 0) and (address < 0x100000000), "address must be 32-bit unsigned number")
+    assert(address % 4 == 0, "address must be aligned")
+    assert(self.memory ~= nil, "memory is not initialized")
+    assert(self.memory[address] ~= nil, "address is not allocated")
+    return self.memory[address]
+end
+
+function RiscVCore:WriteDWORD(address, value)
+    assert((address >= 0) and (address < 0x100000000), "address must be 32-bit unsigned number")
+    assert((value >= 0) and (value < 0x100000000), "value must be 32-bit unsigned number")
+    assert(address % 4 == 0, "address must be aligned")
+    assert(self.memory ~= nil, "memory is not initialized")
+    self.memory[address] = value
+end
 
 function RiscVCore:InitCPU()
     self.registers = {}
@@ -60,6 +99,8 @@ function RiscVCore:InitCPU()
         handler = BaseInstructions_JALR
     }
 
+    self.memory = {}
+
     self.program = {}
     self.program[0] = 5476535 -- LUI x1, 1337
 
@@ -71,23 +112,71 @@ function RiscVCore:Step()
     local instruction = self.program[self.registers.pc]
     assert(instruction ~= nil, "out of bound execution")
 
-    opcode = bit.band(instruction, 127)
+    opcode = bit.band(instruction, 0x7f)
 
     assert(self.opcodes[opcode], "opcode ".. tostring(opcode) .." is not implemented")
 
     if self.opcodes[opcode].type == "U" then
-        local dest = bit.rshift(bit.band(instruction, 3968), 7) -- (instr & 0b111110000000) >> 7
-        local imm_value = bit.band(instruction, 4294963200) -- instr & 0b11111111111111111111000000000000
-        self.opcodes[opcode].handler(self, dest, imm_value)
+        
+        local rd = decode_rd(instruction)
+        local imm_value = bit.band(instruction, 0xfffff000) -- instr & 0xfffff000
+
+        self.opcodes[opcode].handler(self, rd, imm_value)
+
     elseif self.opcodes[opcode].type == "J" then
-        local offset = bit.rshift(bit.band(instruction, 4294963200), 12)*2 -- (instr & 0b11111111111111111111000000000000) >> 12
-        self.opcodes[opcode].handler(self, offset)
+
+        local rd = decode_rd(instruction)
+        local imm_value = bit.band(bit.rshift(instruction, 20), 0x7fe)
+        imm_value = bit.bor(imm_value, bit.band(bit.rshift(instruction, 9), 0x800))
+        imm_value = bit.bor(imm_value, bit.band(instruction, 0xff000))
+        imm_value = bit.bor(imm_value, bit.band(bit.rshift(instruction, 11), 0x100000))
+        imm_value = set_sign(imm_value, 21)
+        
+        self.opcodes[opcode].handler(self, rd, imm_value)
+
     elseif self.opcodes[opcode].type == "I" then
-        local dest = bit.rshift(bit.band(instruction, 3968), 7) -- (instr & 0b111110000000) >> 7
-        local funct3 = bit.rshift(bit.band(instruction, 28672), 12) -- (instr & 0b111000000000000) >> 12
-        local source = bit.rshift(bit.band(instruction, 1015808), 15) -- (instr & 0b11111000000000000000) >> 15
-        local imm_value = bit.rshift(bit.band(instruction, 4293918720), 20) -- (instr & 0b11111111111100000000000000000000) >> 20
-        self.opcodes[opcode].handler(self, dest, funct3, source, imm_value)
+
+        local rd = decode_rd(instruction)
+        local funct3 = decode_funct3(instruction)
+        local rs1 = decode_rs1(instruction)
+        local imm_value = bit.band(bit.rshift(instruction, 20), 0xfff) -- (instr >> 20) & 0xfff
+        imm_value = set_sign(imm_value, 12)
+        
+        self.opcodes[opcode].handler(self, rd, funct3, rs1, imm_value)
+
+    elseif self.opcodes[opcode].type == "B" then
+
+        local funct3 = decode_funct3(instruction)
+        local rs1 = decode_rs1(instruction)
+        local rs2 = decode_rs2(instruction)
+        local imm_value = bit.band(bit.rshift(instruction, 7), 0x1e)
+        imm_value = bit.bor(imm_value, bit.band(bit.rshift(instruction, 20), 0x7e0))
+        imm_value = bit.bor(imm_value, bit.band(bit.lshift(instruction, 4), 0x800))
+        imm_value = bit.bor(imm_value, bit.band(bit.rshift(instruction, 19), 0x1000))
+        imm_value = set_sign(imm_value, 13)
+
+        self.opcode[opcode].handler(self, funct3, rs1, rs2, imm_value)
+
+    elseif self.opcodes[opcode].type == "S" then
+
+        local funct3 = decode_funct3(instruction)
+        local rs1 = decode_rs1(instruction)
+        local rs2 = decode_rs2(instruction)
+        local imm_value = bit.bor(bit.lshift(bit.rshift(instruction, 25), 5), decode_rd(instruction))
+        imm_value = set_sign(imm_value, 12)
+
+        self.opcodes[opcode].handler(self, funct3, rs1, rs2, imm_value)
+
+    elseif self.opcodes[opcode].type == "R" then
+
+        local rd = decode_rd(instruction)
+        local funct3 = decode_funct3(instruction)
+        local rs1 = decode_rs1(instruction)
+        local rs2 = decode_rs2(instruction)
+        local funct7 = bit.rshift(instruction, 25)
+
+        self.opcodes[opcode].handler(self, rd, funct3, rs1, rs2, funct7)
+
     else
         assert(0, "opcode encoding " .. tostring(self.opcodes[opcode].type) .. " is not implemented")
     end
