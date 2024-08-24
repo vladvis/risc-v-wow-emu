38d5251224492556338a214d0c5011a21ab5411f
Maxim Kochukov <kochukov.ma@gmail.com>
2024-08-24 12:04:14 +0300

    doom new binary

--- tests/testsuite.lua
+++ tests/testsuite.lua
@@ -56,15 +56,15 @@ function RunTests()
         malloc = {
             init = Init_malloc,
             verify = Verify_malloc
-        }]]
+        }
         memset_short = {
             init = Init_memset_short,
             verify = Verify_memset_short
-        }
-        --[[doom = {
+        }]]
+        doom = {
             init = Init_doom,
             verify = Verify_doom
-        }--]]
+        }
     }
 
     for name, test in pairs(tests) do
